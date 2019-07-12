fe43436ba573cb32af7bbe63cc47af5b402ea4f6
Glen Bangkila <hadeinvane@gmail.com>
2019-07-12 15:50:39 +0800

    Lami ang pizza

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-blue-200 w-screen h-screen">
-    <h1>Hello World from Webteractive Team</h1>
+  <div class="bg-blue-200 w-screen h-screen flex items-center justify-center">
+    <h1 class="text-5xl">Hello World from Webteractive Team</h1>
   </div>
 </template>
